2862e500d5f91219e9e8eaf3608b64ad6d776bd8
Ziming Mao <ziming.mao@yale.edu>
2020-05-04 20:42:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
   data.reduce({}) do |memo, (property, inner_hash)|   #property - symbol, such as :color
     inner_hash.reduce({}) do |memo_1, (specific, names)| #specific - symbol, such as :purple  names - array ["Queenie", "Alex"]
       names.each do |name|  #name should be a string
-        if n{name.to_sym} == nil 
+        if n{name.to_sym} == nil
           crap = {}
           crap[property] = [specific.to_s]
           n[name.to_sym] = crap
