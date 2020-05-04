cc93207f29c52787aa5214f46565e20d539267b3
Ziming Mao <ziming.mao@yale.edu>
2020-05-04 20:08:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  n = {}
+
+  data.reduce({}) do |memo, (property, inner_hash)|
+    inner_hash({}) do |memo_1, (specific, names)|]
+      
+      names.each do |name|
+        q = n{name.to_sym}
+        q{property} = specific
+        n{name.to_sym} = q
+      end
+      
+    memo_1
+    end
+
+  memo
+  end
+
+  n
+
+  
 end
